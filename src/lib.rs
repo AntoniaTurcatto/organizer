91dc46c7f6b8354291592b5e8368e882d0d076aa
antoniaturcatto <antoniaturcatto29@gmail.com>
2026-02-28 19:21:27 -0300

    Filtering and organizing done

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,106 @@
-pub mod filter{
-    pub const FILTER_EXT: &str = "ext";
-    pub const FILTER_DATE: &str = "date";
-    pub enum Filter{
+use std::error::Error;
+use std::fs::DirEntry;
+use std::path::PathBuf;
+use std::{fs, io};
+use std::ffi::OsString;
+
+use chrono::{DateTime, Datelike, Local};
+
+use crate::filters::{DateFilter, Filter};
+
+pub type RecoverableErrorVec = Vec<Box<dyn Error>>;
+
+pub mod filters {
+    pub enum Filter {
         Ext,
         Date(DateFilter),
     }
 
-    pub enum DateFilter{
+    pub enum DateFilter {
         Day,
         Month,
         Year,
     }
+}
 
-    impl Filter{
-        pub fn build<'a>(filter: &str) -> Self{
-            if filter != FILTER_EXT && filter != FILTER_DATE{
-                panic!("Invalid filter value");
+pub fn filter_folder(
+    path: &str,
+    filter: &filters::Filter,
+) -> Result<Option<RecoverableErrorVec>, Box<dyn Error>> {
+    if !fs::exists(path)? {
+        return Err(Box::new(io::Error::new(
+            io::ErrorKind::NotFound,
+            format!("Diretório não encontrado: {}", path),
+        )));
+    }
+
+    let folder_entities = fs::read_dir(path)?;
+    let mut errors: Vec<Box<dyn Error>> = Vec::new();
+    for entities_res in folder_entities {
+        match entities_res {
+            Err(e) => {
+                errors.push(Box::new(e));
             }
-
-            if filter == FILTER_EXT {
-                Filter::Ext
-            } else{
-                Filter::Date(DateFilter::Day)
+            Ok(dir_entry) => {
+                if let Err(e) = filter_dir_entry(dir_entry, filter) {
+                    errors.push(e);
+                }
             }
         }
     }
-
-    impl DateFilter{
-        //TODO: implement build method
+    if errors.is_empty(){
+        Ok(None)
+    } else {
+        Ok(Some(errors))
     }
-
 }
 
-pub mod file{
+fn filter_dir_entry(dir_entry: DirEntry, filter: &Filter) -> Result<(), Box<dyn Error>> {
+    if !dir_entry.file_type()?.is_file() {
+        return Ok(());
+    }
+    let (orig_path, target_path) = get_parent_folder_and_target(&dir_entry, filter)?;
 
-    pub struct File{
-        pub file_type: FileType,
-        pub date: Date,
+    if let Some(parent) = target_path.parent() {
+        if !parent.exists() {
+            fs::create_dir_all(parent)?;
+        }
     }
 
-    pub enum FileType{
-        Image,
-        Audio,
-        Text,
-    }
+    fs::rename(orig_path, target_path)?;
+    Ok(())
+}
 
-    pub struct Date{
-        pub day: u8,
-        pub month: u8,
-        pub year: u32, 
-    }
-    
+fn get_parent_folder_and_target(dir_entry: &DirEntry, filter: &Filter) -> Result<(PathBuf, PathBuf), Box<dyn Error>>{
+    let orig_path_binding = dir_entry.path();
+
+    let target = match filter {
+        Filter::Ext => {
+            orig_path_binding
+                .extension()
+                .ok_or("Could not get file extension")?
+                .to_os_string()
+        }
+        Filter::Date(date_filter)=>{
+            let sys_time = orig_path_binding
+                .metadata()?
+                .modified()?;
+            let date_time: DateTime<Local> = DateTime::from(sys_time);
+            let target_num = match date_filter {
+                DateFilter::Day   => date_time.day() as i32,
+                DateFilter::Month => date_time.month() as i32,
+                DateFilter::Year  => date_time.year(),
+            };
+            OsString::from(target_num.to_string())                
+        }
+    };
+
+    let parent_folder = orig_path_binding
+        .parent()
+        .ok_or("Cound not get parent folder")?
+        .to_path_buf();
+
+    let target_folder = parent_folder.join(target);
+    let target_path = target_folder.join(orig_path_binding.file_name().ok_or("File must have a name")?);
+    Ok((orig_path_binding, target_path))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
-use clap::command;
-use clap::arg;
+use ::std::process;
+use clap::{arg, command};
+use organizer::filters::{DateFilter, Filter};
 
 fn main() {
+    let (filter, path) = init();
+    match organizer::filter_folder(&path, &filter) {
+        Err(e) =>{
+            eprintln!("Could not organize provided folder: {e}");
+            process::exit(1);
+        }
+        Ok(recv_errors_op)=>{
+            println!("Done!");
+            if let Some(recv_errors) = recv_errors_op{
+                println!("Obtained some errors:");
+                for error in recv_errors{
+                    println!("{error}");
+                }
+            }
+        }
+    }
+}
+
+fn init() -> (Filter, String) {
+    const DAY: &str = "d";
+    const MONTH: &str = "m";
+    const YEAR: &str = "y";
+
     let matches = command!()
-        .arg(arg!(--filter <TYPE>).required(true))
-        .arg(arg!(--time <"TIME-FILTER">).value_parser(["d", "m", "y"]))
+        .arg(arg!(--folder <PATH>).required(true))
+        .arg(arg!(--time <"TIME-FILTER">).value_parser([DAY, MONTH, YEAR]))
         .get_matches();
 
-    let filter_type = matches.get_one::<String>("filter").expect("filter expected.");
-    //TODO: build Filter
+    let filter = if let Some(time_spec) = matches.get_one::<String>("time") {
+        if time_spec == DAY {
+            Filter::Date(DateFilter::Day)
+        } else if time_spec == MONTH {
+            Filter::Date(DateFilter::Month)
+        } else {
+            Filter::Date(DateFilter::Year)
+        }
+    } else {
+        Filter::Ext
+    };
 
+    (filter, matches.get_one::<String>("folder").unwrap().to_owned())
 }
